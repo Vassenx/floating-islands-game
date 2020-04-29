936d6f53ed342f08e553f8ffe1db76bc2d622dc3
Samantha Difeo <sdifeo1@gmail.com>
2020-04-29 02:08:35 -0400

    Add unity, basic player and camera controllers

---
+++ FloatingIslands/Assets/Scripts/CameraController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: cinemachine?
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float lerpForwardDuration = 0.5f;
+
+    //TODO: only move with mouse click
+
+    /*[SerializeField] float pitchSpeed = 100f;
+    [SerializeField] private float yawSpeed = 100f;
+
+    private float xRot = 0.0f;
+
+     private void Update()
+    {
+        float yaw = yawSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+        float pitch = pitchSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        xRot += pitch;
+
+        if (xRot > 90)
+        {
+            xRot = 90.0f;
+            pitch = 0.0f;
+            Clamp(270f);
+        }
+        else if (xRot < -90)
+        {
+            xRot = -90.0f;
+            pitch = 0.0f;
+            Clamp(90f);
+        }
+
+        //vertical movement was finicky as could look at goofy angles
+        //transform.Rotate(-transform.right * pitch);
+       // transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        //player.transform.Rotate(Vector3.up * yaw);
+    }
+
+    private void Clamp(float clampVal)
+    {
+        Vector3 eulerRot = transform.eulerAngles;
+        eulerRot.x = clampVal;
+        transform.eulerAngles = eulerRot;
+    }*/
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+
+        StartCoroutine(RotateToPlayer(transform.forward, player.transform.forward));
+    }
+
+    IEnumerator RotateToPlayer(Vector3 camPos, Vector3 playerPos)
+    {
+        for (float t = 0f; t < lerpForwardDuration; t += Time.deltaTime)
+        {
+            transform.forward = Vector3.Slerp(camPos, playerPos, t / lerpForwardDuration);
+            yield return 0;
+        }
+    }
+}
---
+++ FloatingIslands/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    /*Basic movement*/
+    [SerializeField] private CharacterController charController;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotSpeed = 50f;
+    private Vector3 movement = Vector3.zero;
+
+    /*Jump*/
+    [SerializeField] private float jumpSpeed = 0.2f;
+    [SerializeField] private float jumpDuration = 3f; //how long the jump can last
+    [SerializeField] private float jumpCooldown = 2f; //how long til next jump
+    [SerializeField] private float jumpDampening = 0.1f;
+    private float curJumpSpeed;
+    private float curJumpDuration = 0f;
+    private float curJumpCooldown = 0f;
+
+    /*Velocity = external forces here*/
+    private Vector3 velocity = Vector3.zero; //for jump/gravity
+    [SerializeField] private float gravity = 9f;
+
+    /*Grounding player*/
+    private float interactGroundRadius = 0.5f;
+    private bool isGrounded = false;
+    private float halfHeightOfPlayer;
+    [SerializeField] private LayerMask groundMask;
+
+    private void Start()
+    {
+        halfHeightOfPlayer = GetComponent<Collider>().bounds.extents.y;
+        groundMask = LayerMask.NameToLayer("Ground");
+        curJumpSpeed = jumpSpeed;
+    }
+
+    void Update()
+    {
+        BasicMovement();
+
+        isGrounded = Physics.CheckSphere(transform.position - Vector3.up * halfHeightOfPlayer, interactGroundRadius, groundMask, QueryTriggerInteraction.Ignore);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        Jump();
+
+        velocity.y -= gravity;
+        charController.Move(velocity * Time.deltaTime);
+
+    }
+
+    //rotate for horizontal, move forward/backwards for vertical
+    private void BasicMovement()
+    {
+        var yaw = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(yaw) > Mathf.Epsilon)
+        {
+            transform.Rotate(Vector3.up, yaw * Time.deltaTime * rotSpeed);
+        }
+        else
+        {
+            yaw = 0f;
+        }
+
+        movement = Input.GetAxis("Vertical") * transform.forward;
+        if (movement != Vector3.zero)
+        {
+            charController.Move(movement * Time.deltaTime * speed);
+        }
+    }
+
+    //TODO: touch up this mechanic
+    //like a rocket pack jump
+    private void Jump()
+    {
+
+        //continue jumping
+        if (Input.GetKey(KeyCode.Space) && curJumpDuration < jumpDuration)
+        {
+            curJumpDuration += Time.deltaTime;
+
+            //cancel gravity, not realistic, but gravity is annoying
+            velocity.y += gravity * Time.deltaTime;
+
+            curJumpSpeed = Mathf.Max(0.1f, curJumpSpeed - jumpDampening);
+            velocity.y += curJumpSpeed;
+        }
+        else
+        {
+            if (curJumpCooldown >= jumpCooldown)
+            {
+                curJumpCooldown = 0;
+                curJumpDuration = 0;
+                curJumpSpeed = jumpSpeed;
+            }
+            else
+            {
+                curJumpCooldown += Time.deltaTime;
+            }
+        }
+    }
+
+}
